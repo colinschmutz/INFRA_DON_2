49fbb154ab509c456fb4bafc71f75713a50d633f
Colin Schmutz <colin.schmutz@heig-vd.ch>
2024-10-31 15:31:58 +0100

    linking vue.js and couchDB

---
+++ vue-project/src/views/StructureCouchDB.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+
+export default {
+
+  data() {
+    return {
+      datas: [],
+      databaseReference: null,
+    };
+  },
+
+  methods: {
+
+    inc() {
+
+    },
+
+    initDatabase() {
+      // lancer une initialisation de la base de données
+      // Si la connexion est ok, alors je fais un fetchdata
+      const db = new PouchDB('http://127.0.0.1:5984/_utils/#database/zoulets/_all_docs');
+
+    },
+
+    fetchData() {
+      // récupérer tous les documents de ma collection
+      db.allDocs([options], [callback])
+    }
+
+  },
+
+  mounted() {
+    this.initDatabase()
+  }
+
+}
+</script>
+
+<template>
+  <h1>InfraDon2</h1>
+  <p>Counter: {{ datas }}</p>
+  <button type="button" role="button" @click="inc">+1</button>
+</template>
